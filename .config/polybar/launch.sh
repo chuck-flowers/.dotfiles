8c3d5402751d8a027c7f70dfb4651e23ba494abb
Chuck Flowers <t_chuck_flowers@yahoo.com>
2021-07-11 22:53:13 -0400

    Shows polybars on all monitors and adds i3 gaps around polybars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,13 @@
 
 killall -q polybar
 
-polybar desktop-bottom 2>&1 & disown
-polybar desktop-top 2>&1 & disown
+if type "xrandr"; then
+	for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+		MONITOR=$m polybar --reload desktop-bottom &
+		MONITOR=$m polybar --reload desktop-top &
+	done
+else
+	polybar --reload desktop-bottom &
+	polybar --reload desktop-top &
+fi
 
