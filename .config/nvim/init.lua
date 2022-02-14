c04883e8e4291762f855aaf3468dc2db9ef4403c
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-02-14 10:21:05 -0500

    Adds a keybinding for text searching an nvim project

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -109,6 +109,7 @@ vim.api.nvim_set_keymap('n', '<Leader>N', ':NERDTreeToggle<CR>', { noremap = tru
 -- Configure telescope
 vim.api.nvim_set_keymap('n', '<Leader>ff', ':Telescope find_files<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope git_branches<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>ft', ':Telescope grep_string<CR>', { noremap = true })
 telescope.setup({
 	defaults = {
 		file_ignore_patterns = { 'node_modules' }
