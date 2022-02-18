a1210dad014de3e606a2240db27b06018e94e217
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-02-18 10:00:10 -0500

    Adds additional keybindings for git in neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,6 +60,8 @@ cmp.setup({
 vim.api.nvim_set_keymap('n', '<Leader>gs', ':Git<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gc', ':Git commit<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gl', ':Git log --all --decorate --oneline --graph<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>gbm', ':Git checkout --track master -b ', { noremap = true })
+vim.api.nvim_set_keymap('n', '<Leader>gir', ':Git rebase -i<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gr', ':Git rebase<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gp', ':Git push<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>gP', ':Git pull<CR>', { noremap = true })
