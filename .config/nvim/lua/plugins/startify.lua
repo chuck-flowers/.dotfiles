9f1ab9e414d52287d52f7f980c84938b581592cd
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-17 20:58:34 -0400

    Adds qtile to the startify bookmarks

--- .config/nvim/lua/plugins/startify.lua
+++ .config/nvim/lua/plugins/startify.lua
@@ -1,4 +1,8 @@
-vim.g.startify_bookmarks = { { c = '~/.config/nvim/init.lua' } }
+vim.g.startify_bookmarks = {
+	{ c = '~/.config/nvim/init.lua' },
+	{ w = '~/.config/qtile/config.py' }
+}
+
 vim.g.startify_lists = {
 	{ type = 'sessions', header = {'Projects'} },
 	{ type = 'bookmarks', header = {'Bookmarks'} }
