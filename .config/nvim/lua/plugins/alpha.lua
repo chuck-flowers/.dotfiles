9fb942131abe3e9a7a4fe4952a459d3ef3316a29
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-17 21:39:23 -0400

    Adds a keybinding to go back to the alpha page

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,5 +1,7 @@
 local alpha = require('alpha')
 local alpha_startify = require('alpha.themes.startify')
 
+vim.api.nvim_set_keymap('n', '<Leader>a', '<cmd>Alpha<CR>', { noremap = true })
+
 alpha.setup(alpha_startify.config)
 
