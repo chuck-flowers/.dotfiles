d550d8e19ae0f8ea1ce502562ceb45d673c96d7f
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-21 12:05:07 -0400

    Uses packer to update and install itself

---
+++ .config/nvim/lua/plugins/packer.lua
@@ -0,0 +1,9 @@
+return {
+	'wbthomason/packer.nvim',
+	config = function()
+		vim.api.nvim_set_keymap('n', '<Leader>ps', '<cmd>PackerStatus<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>pc', '<cmd>PackerCompile<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>pi', '<cmd>PackerInstall<CR>', { noremap = true })
+	end
+}
+
