32d9d068c83e43af88168de8354339795a1371e8
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-21 15:06:34 -0400

    Properly configures `trouble` in neovim

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -2,6 +2,12 @@ return {
 	'folke/trouble.nvim',
 	config = function()
 		local trouble = require('trouble')
+
+		vim.api.nvim_set_keymap('n', '<Leader>t', '<cmd>TroubleToggle<CR>', { noremap = true })
+
+		trouble.setup({
+
+		})
 	end
 }
 
