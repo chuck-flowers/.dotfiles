cd46f077114f713e0be31694f378e94ddb263c10
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-21 15:06:55 -0400

    Adds a keybinding for `:PackerUpdate` in neovim

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -4,6 +4,7 @@ return {
 		vim.api.nvim_set_keymap('n', '<Leader>ps', '<cmd>PackerStatus<CR>', { noremap = true })
 		vim.api.nvim_set_keymap('n', '<Leader>pc', '<cmd>PackerCompile<CR>', { noremap = true })
 		vim.api.nvim_set_keymap('n', '<Leader>pi', '<cmd>PackerInstall<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>pu', '<cmd>PackerUpdate<CR>', { noremap = true })
 	end
 }
 
