5e0a4ca2a7e5742ab07d240d2f5688ec82f2d9dd
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-22 10:48:27 -0400

    Adds additional orgmode configuration

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -6,8 +6,13 @@ return {
 	config = function()
 		local orgmode = require('orgmode')
 
-		orgmode.setup({
+		orgmode.setup_ts_grammar()
 
+		orgmode.setup({
+			org_agenda_files = {
+				'~/org/*'
+			},
+			org_default_notes_file = '~/org/notes.org'
 		})
 	end
 }
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,16 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
-	run = ':TSUpdate'
+	run = ':TSUpdate',
+	config = function()
+		local ts_configs = require('nvim-treesitter.configs')
+
+		ts_configs.setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = { 'org' }
+			},
+			ensure_installed = { 'org' }
+		})
+	end
 }
 
