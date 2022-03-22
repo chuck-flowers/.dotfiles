e4900ea163d162d6c86d86ea029fd0238fa7e33c
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-22 10:56:36 -0400

    Adds a keybinding for closing NERDTree in neovim

--- .config/nvim/lua/plugins/nerdtree.lua
+++ .config/nvim/lua/plugins/nerdtree.lua
@@ -3,6 +3,7 @@ return {
 	config = function()
 		vim.api.nvim_set_keymap('n', '<Leader>nn', ':NERDTreeToggle<CR>', { noremap = true })
 		vim.api.nvim_set_keymap('n', '<Leader>nf', ':NERDTreeFind<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>nc', ':NERDTreeClose<CR>', { noremap = true })
 	end
 }
 
