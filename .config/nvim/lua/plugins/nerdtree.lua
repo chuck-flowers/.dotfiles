30a4efa98a1cd875463d0ff4a80665f1d5738a35
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-23 01:34:42 -0400

    Replaces nerdtree with nvim-tree

--- .config/nvim/lua/plugins/nerdtree.lua
+++
@@ -1,9 +0,0 @@
-return {
-	'preservim/nerdtree',
-	config = function()
-		vim.api.nvim_set_keymap('n', '<Leader>nn', ':NERDTreeToggle<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>nf', ':NERDTreeFind<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>nc', ':NERDTreeClose<CR>', { noremap = true })
-	end
-}
-
---
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,14 @@
+return {
+	'kyazdani42/nvim-tree.lua',
+	requires = {
+		'kyazdani42/nvim-web-devicons'
+	},
+	config = function()
+		local nvim_tree = require('nvim-tree')
+
+		vim.api.nvim_set_keymap('n', '<Leader>nn', '<cmd>NvimTreeToggle<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>nr', '<cmd>NvimTreeRefresh<CR>', { noremap = true })
+
+		nvim_tree.setup({})
+	end
+}
