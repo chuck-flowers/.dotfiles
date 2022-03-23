27346ee2c417e5c5a7e6183cbbfb9167fe2734c0
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-23 01:42:18 -0400

    Switches from `fugitive-vim` to `neogit`

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,16 @@
+return {
+	'TimUntersberger/neogit',
+	config = function()
+		vim.api.nvim_set_keymap('n', '<Leader>gs', ':Neogit<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gc', ':Git commit<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gC', ':Git commit -a<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gl', ':Git log --all --decorate --oneline --graph<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gbm', ':Git checkout --track master -b ', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gir', ':Git rebase -i<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gr', ':Git rebase<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gm', ':Git merge ', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gp', ':Git push<CR>', { noremap = true })
+		-- vim.api.nvim_set_keymap('n', '<Leader>gP', ':Git pull<CR>', { noremap = true })
+	end
+}
+
