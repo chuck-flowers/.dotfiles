ab3494f5787036b4beea0e80a579f4863452825b
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-24 20:38:31 -0400

    Adds keybinding to show current file in nvim-tree

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -7,6 +7,7 @@ return {
 		local nvim_tree = require('nvim-tree')
 
 		vim.api.nvim_set_keymap('n', '<Leader>nn', '<cmd>NvimTreeToggle<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>nf', '<cmd>NvimTreeFindFile<CR>', { noremap = true })
 		vim.api.nvim_set_keymap('n', '<Leader>nr', '<cmd>NvimTreeRefresh<CR>', { noremap = true })
 
 		nvim_tree.setup({})
