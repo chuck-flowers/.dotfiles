114665407d736cf1b18792e43d3f83e719e7b66e
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-03-24 20:38:03 -0400

    Adds git commit and git log keybinds to neogit

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,10 +1,11 @@
 return {
 	'TimUntersberger/neogit',
+	requires = { 'nvim-lua/plenary.nvim' },
 	config = function()
-		vim.api.nvim_set_keymap('n', '<Leader>gs', ':Neogit<CR>', { noremap = true })
-		-- vim.api.nvim_set_keymap('n', '<Leader>gc', ':Git commit<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>gs', '<cmd>lua require("neogit").open()<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>gc', '<cmd>lua require("neogit").open({ "commit" })<CR>', { noremap = true })
 		-- vim.api.nvim_set_keymap('n', '<Leader>gC', ':Git commit -a<CR>', { noremap = true })
-		-- vim.api.nvim_set_keymap('n', '<Leader>gl', ':Git log --all --decorate --oneline --graph<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>gl', '<cmd>lua require("neogit").open({ "log" })<CR>', { noremap = true })
 		-- vim.api.nvim_set_keymap('n', '<Leader>gbm', ':Git checkout --track master -b ', { noremap = true })
 		-- vim.api.nvim_set_keymap('n', '<Leader>gir', ':Git rebase -i<CR>', { noremap = true })
 		-- vim.api.nvim_set_keymap('n', '<Leader>gr', ':Git rebase<CR>', { noremap = true })
