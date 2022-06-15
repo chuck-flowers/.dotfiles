1acec16e90dd3cc052b32b8d71363b0929b97df0
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-06-15 11:03:27 -0400

    Adds bootstraping to packer.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -15,13 +15,34 @@ local function ls_files(directory)
 	return to_return
 end
 
+
+local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
+	packerClone = vim.fn.system({
+		'git',
+		'clone',
+		'--depth',
+		'1',
+		'https://github.com/wbthomason/packer.nvim',
+		packer_install_path
+	})
+
+	packer.sync()
+end
+
 local plugin_config_dir = os.getenv('HOME') .. '/.config/nvim/lua/plugins'
 local plugin_config_files = ls_files(plugin_config_dir);
 packer.startup(function (use)
+	-- Use each packer definition in the plugins folder
 	for i, pluginFile in ipairs(plugin_config_files) do
 		local pluginName = string.gsub(pluginFile, '.lua', '')
 		local pluginConfig = require('plugins/' .. pluginName)
 		use(pluginConfig)
+	end
+
+	-- If this is a bootstrap run, automatically sync
+	if packerClone then
+		packer.sync()
 	end
 end)
 
