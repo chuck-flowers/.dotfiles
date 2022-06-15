0fadda5dd686e44abc9113d0cb15fe47ba12c3af
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-06-15 17:02:41 -0400

    Adds neovim keybindings for moving lines of text

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,3 +1,12 @@
 vim.g.mapleader = " "
+
+-- Cycle through the quick fix list
 vim.api.nvim_set_keymap('n', '<C-h>', ':cprevious<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-l>', ':cnext<CR>', { noremap = true })
+
+-- Move lines of text
+vim.api.nvim_set_keymap('n', '<C-j>', ':m+1<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-k>', ':m-2<CR>==', { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-j>', ":'<'>m '>+1<CR>gv=gv", { noremap = true })
+vim.api.nvim_set_keymap('v', '<C-k>', ":'<'>m '<-2<CR>gv=gv", { noremap = true })
+
