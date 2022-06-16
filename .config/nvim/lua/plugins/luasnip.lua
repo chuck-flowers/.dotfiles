15c26f8457c88a7478cb961223ca9099ea919b8e
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-06-16 17:13:49 -0400

    Reworks the LuaSnip snippets

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,30 +1,47 @@
 return {
 	'L3MON4D3/LuaSnip',
 	config = function()
+		-- Modules
 		local luasnip = require('luasnip')
+		local luasnip_fmt = require('luasnip.extras.fmt')
 
+		-- LS Functions
 		local s = luasnip.snippet
 		local jump = luasnip.jump
 		local jumpable = luasnip.jumpable
+		local fmt = luasnip_fmt.fmt
 
-		-- Nodes
+		-- LS Nodes
 		local t = luasnip.text_node
 		local i = luasnip.insert_node
 
 		luasnip.add_snippets('typescript', {
 			s(
 				'export_function',
-				{
-					t('export function '),
-					i(1, 'function_name'),
-					t(' ('),
-					i(2, 'params'),
-					t('): '),
-					i(3, 'return_type'),
-					t({ ' {', '' }),
-					i(0, 'body'),
-					t({ '', '}' })
-				}
+				fmt(
+					'export function {}({}): {} {{\n\t{}\n}}',
+					{
+						i(1, 'name'),
+						i(2, 'params'),
+						i(3, 'return_type'),
+						i(0, 'body')
+					}
+				)
+			)
+		})
+
+		luasnip.add_snippets('typescript', {
+			s(
+				'export_async_function',
+				fmt(
+					'export async function {}({}): Promise<{}> {{\n\t{}\n}}',
+					{
+						i(1, 'name'),
+						i(2, 'params'),
+						i(3, 'return_type'),
+						i(0, 'body')
+					}
+				)
 			)
 		})
 
