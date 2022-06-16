f9a7177a0a0543e52f5150c53fa6ecf9e0599016
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-06-16 13:37:17 -0400

    Initial working version of LuaSnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -2,6 +2,26 @@ return {
 	'L3MON4D3/LuaSnip',
 	config = function()
 		local luasnip = require('luasnip')
+		local s = luasnip.snippet
+		local t = luasnip.text_node
+		local i = luasnip.insert_node
+
+		luasnip.add_snippets('typescript', {
+			s(
+				'export_function',
+				{
+					t('export function '),
+					i(1, 'function_name'),
+					t(' ('),
+					i(2, 'params'),
+					t('): '),
+					i(3, 'return_type'),
+					t({ ' {', '' }),
+					i(0, 'body'),
+					t({ '', '}' })
+				}
+			)
+		})
 	end
 }
 
