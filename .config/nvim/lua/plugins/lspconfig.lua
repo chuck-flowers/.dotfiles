44e6d5a375e31585d8a0c529e4265b2465344b5e
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-06-30 00:09:12 -0400

    Adds a lua LSP to neovim

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -19,6 +19,21 @@ return {
 			},
 			pylsp = {},
 			rust_analyzer = {},
+			sumneko_lua = {
+				settings = {
+					Lua = {
+						runtime = {
+							version = 'LuaJIT'
+						},
+						diagnostics = {
+							globals = { 'vim' }
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true)
+						}
+					}
+				}
+			},
 			tsserver = {},
 			yamlls = {
 				settings = {
