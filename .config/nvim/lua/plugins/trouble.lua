efa793d6991110b3e0874c818bfede5017f8b37c
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-07-15 08:59:24 -0400

    Tweaks keybindings for neovim's trouble

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -3,7 +3,9 @@ return {
 	config = function()
 		local trouble = require('trouble')
 
-		vim.api.nvim_set_keymap('n', '<Leader>t', '<cmd>TroubleToggle<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>tt', '<cmd>TroubleToggle<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>tw', '<cmd>TroubleToggle workspace_diagnostics<CR>', { noremap = true })
+		vim.api.nvim_set_keymap('n', '<Leader>tq', '<cmd>TroubleToggle quickfix<CR>', { noremap = true })
 
 		trouble.setup({
 
