fe879f4b1da6cf11bd4f9139cde0ff228bec8bf2
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-09-01 12:34:44 -0400

    Tweaks the behavior of half-page scrolling in nvim

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -10,3 +10,7 @@ vim.api.nvim_set_keymap('n', '<C-k>', ':m-2<CR>==', { noremap = true })
 vim.api.nvim_set_keymap('v', '<C-j>', ":'<'>m '>+1<CR>gv=gv", { noremap = true })
 vim.api.nvim_set_keymap('v', '<C-k>', ":'<'>m '<-2<CR>gv=gv", { noremap = true })
 
+-- Tweaks the behavior of the scrolling behavior
+vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>M', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>M', { noremap = true })
+
