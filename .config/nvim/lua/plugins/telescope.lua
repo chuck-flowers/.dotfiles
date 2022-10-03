0aa4e65e9f3e7faf3cb3661f470d9f9411d50886
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-03 12:55:01 -0400

    Ignores the `.git` folder in nvim-telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -7,20 +7,22 @@ return {
 	},
 	config = function()
 		local telescope = require('telescope')
-		local builtins = require('telescope.builtin')
 
 		telescope.load_extension('project')
 
-		vim.api.nvim_set_keymap('n', '<Leader>ff', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>fb', ':Telescope git_branches<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>ft', ':Telescope live_grep<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>fp', ':Telescope project<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>fs', ':Telescope lsp_dynamic_workspace_symbols<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>ff', ':lua require("telescope.builtin").find_files({ hidden = true })<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>fb', ':Telescope git_branches<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>ft', ':Telescope live_grep<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>fh', ':Telescope help_tags<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>fp', ':Telescope project<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>fs', ':Telescope lsp_dynamic_workspace_symbols<CR>', { noremap = true })
 
 		telescope.setup({
 			defaults = {
-				file_ignore_patterns = { 'node_modules' }
+				file_ignore_patterns = {
+					'node_modules',
+					'.git'
+				}
 			}
 		})
 	end
