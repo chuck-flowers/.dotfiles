7be65c245bf6fe8740a4ecad464d9e908d2bc41d
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-03 12:55:20 -0400

    CTRL+H/L now cycle through nvim diangostics

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,8 +1,8 @@
 vim.g.mapleader = " "
 
--- Cycle through the quick fix list
-vim.api.nvim_set_keymap('n', '<C-h>', ':cprevious<CR>', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-l>', ':cnext<CR>', { noremap = true })
+-- Cycle through diagnostics
+vim.keymap.set('n', '<C-l>', vim.diagnostic.goto_next, { noremap = true })
+vim.keymap.set('n', '<C-h>', vim.diagnostic.goto_prev, { noremap = true })
 
 -- Move lines of text
 vim.api.nvim_set_keymap('n', '<C-j>', ':m+1<CR>==', { noremap = true })
@@ -13,4 +13,3 @@ vim.api.nvim_set_keymap('v', '<C-k>', ":'<'>m '<-2<CR>gv=gv", { noremap = true }
 -- Tweaks the behavior of the scrolling behavior
 vim.api.nvim_set_keymap('n', '<C-d>', '<C-d>M', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-u>', '<C-u>M', { noremap = true })
-
