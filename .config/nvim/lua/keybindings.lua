f3ceb38fec6ddda26501b750998c65cb25e4dc11
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-07 09:24:18 -0400

    Uses NVIM keybinding API

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -5,10 +5,10 @@ vim.keymap.set('n', '<C-l>', vim.diagnostic.goto_next, { noremap = true })
 vim.keymap.set('n', '<C-h>', vim.diagnostic.goto_prev, { noremap = true })
 
 -- Move lines of text
-vim.api.nvim_set_keymap('n', '<C-j>', ':m+1<CR>==', { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-k>', ':m-2<CR>==', { noremap = true })
-vim.api.nvim_set_keymap('v', '<C-j>', ":'<'>m '>+1<CR>gv=gv", { noremap = true })
-vim.api.nvim_set_keymap('v', '<C-k>', ":'<'>m '<-2<CR>gv=gv", { noremap = true })
+vim.keymap.set('n', '<C-j>', ':m+1<CR>==', { noremap = true })
+vim.keymap.set('n', '<C-k>', ':m-2<CR>==', { noremap = true })
+vim.keymap.set('v', '<C-j>', ":'<'>m '>+1<CR>gv=gv", { noremap = true })
+vim.keymap.set('v', '<C-k>', ":'<'>m '<-2<CR>gv=gv", { noremap = true })
 
 -- Tweaks the behavior of the scrolling behavior
 vim.keymap.set('n', '<C-f>', '<C-f>M', { noremap = true })
