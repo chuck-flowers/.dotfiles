3455df0d6cb2a4b068e57c71963c4e07fd351378
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-11 10:18:22 -0400

    Tweaks NvimTree keybindings

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -5,10 +5,19 @@ return {
 	},
 	config = function()
 		local nvim_tree = require('nvim-tree')
+		local api = require('nvim-tree.api')
 
-		vim.api.nvim_set_keymap('n', '<Leader>nn', '<cmd>NvimTreeToggle<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>nf', '<cmd>NvimTreeFindFile<CR>', { noremap = true })
-		vim.api.nvim_set_keymap('n', '<Leader>nr', '<cmd>NvimTreeRefresh<CR>', { noremap = true })
+		vim.keymap.set('n', '<Leader>nn', function()
+			api.tree.toggle(false, false, vim.fn.getcwd())
+		end)
+		vim.keymap.set('n', '<Leader>nf', function()
+			local curr_file = vim.fn.bufname()
+			api.tree.open()
+			api.tree.find_file(curr_file)
+		end)
+		vim.keymap.set('n', '<Leader>nr', function ()
+			api.tree.reload()
+		end)
 
 		nvim_tree.setup({})
 	end
