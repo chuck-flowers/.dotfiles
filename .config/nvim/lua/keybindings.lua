d8450bb79de831d46ea7f997bea2ac09b3346ac8
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-13 15:18:58 -0400

    Tweaks scrolling behavior of search in nvim

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -10,11 +10,13 @@ vim.keymap.set('n', '<C-k>', ':m-2<CR>==', { noremap = true })
 vim.keymap.set('v', '<C-j>', ":'<'>m '>+1<CR>gv=gv", { noremap = true })
 vim.keymap.set('v', '<C-k>', ":'<'>m '<-2<CR>gv=gv", { noremap = true })
 
--- Tweaks the behavior of the scrolling behavior
+-- Tweaks the scrolling behavior
 vim.keymap.set('n', '<C-f>', '<C-f>M', { noremap = true })
 vim.keymap.set('n', '<C-b>', '<C-b>M', { noremap = true })
 vim.keymap.set('n', '<C-d>', '<C-d>M', { noremap = true })
 vim.keymap.set('n', '<C-u>', '<C-u>M', { noremap = true })
+vim.keymap.set('n', 'n', 'nzz')
+vim.keymap.set('n', 'N', 'Nzz')
 
 -- Allows for reloading config
 vim.keymap.set('n', '<leader><leader>so', function ()
