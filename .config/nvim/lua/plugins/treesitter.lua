1fb02e41bcd5b1a3674ecb41d6e7bcf9ac1b569e
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-13 10:02:39 -0400

    Extends treesitter functionality in nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,22 @@ return {
 	'nvim-treesitter/nvim-treesitter',
 	run = ':TSUpdate',
 	requires = {
-		'nvim-treesitter/nvim-treesitter-context'
+		'nvim-treesitter/nvim-treesitter-context',
+		'nvim-treesitter/nvim-treesitter-textobjects',
+		'nvim-treesitter/playground'
 	},
 	config = function()
 		local ts_configs = require('nvim-treesitter.configs')
 		local ts_context = require('treesitter-context')
 
 		ts_configs.setup({
+			ensure_installed = {
+				'lua',
+				'query',
+				'org',
+				'tsx',
+				'typescript'
+			},
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = { 'org' }
@@ -22,11 +31,24 @@ return {
 					node_decremental = '<C-H>'
 				}
 			},
-			ensure_installed = {
-				'lua',
-				'org',
-				'tsx',
-				'typescript'
+			playground = {
+				enable = true
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					keymaps = {
+						-- Functions
+						['af'] = '@function.outer',
+						['if'] = '@function.inner',
+
+						-- Classes
+						['ac'] = '@class.outer',
+						['ic'] = '@class.inner'
+					}
+				},
+				swap = {},
+				move = {}
 			}
 		})
 
