c31c71e11c4119110c11ec89dc5d777c2569d006
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-14 17:49:43 -0400

    Adds a TypeScript template for a functional react component

--- .config/nvim/lua/plugins/luasnip/snippets/typescript.lua
+++ .config/nvim/lua/plugins/luasnip/snippets/typescript.lua
@@ -12,8 +12,18 @@ local export_async_function = fmt('export async function {}({}): Promise<{}> {{\
 	i(0, 'body')
 })
 
+local react_fn_cmp = fmt('export type {propsName} = {{\n\n}};\n\nexport function {componentName}(props: {propsName}) {{\n\t{body}\n}}', {
+	propsName = f(function (args)
+		local component_name = args[1][1]
+		return component_name .. 'Props'
+	end, { 1 }),
+	componentName = i(1, 'component_name'),
+	body = i(0, 'body')
+})
+
 return {
 	s('export_function', export_function),
-	s('export_async_function', export_async_function)
+	s('export_async_function', export_async_function),
+	s('react_fn_cmp', react_fn_cmp)
 }
 
