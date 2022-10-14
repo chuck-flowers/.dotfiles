8ca759823fbfebca50b3eaa2344e1d697d11a6be
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-14 13:09:55 -0400

    Lazy loads the nvim-tree plugin

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,24 +1,37 @@
 return {
-	'kyazdani42/nvim-tree.lua',
+	'nvim-tree/nvim-tree.lua',
 	requires = {
-		'kyazdani42/nvim-web-devicons'
+		'nvim-tree/nvim-web-devicons'
 	},
 	config = function()
-		local nvim_tree = require('nvim-tree')
-		local api = require('nvim-tree.api')
+		local nvim_tree = nil
+		local api = nil
+		local function lazy_load()
+			if nvim_tree == nil then
+				nvim_tree = require('nvim-tree')
+				nvim_tree.setup({})
+
+				api = require('nvim-tree.api')
+			end
+		end
 
 		vim.keymap.set('n', '<Leader>nn', function()
-			api.tree.toggle(false, false, vim.fn.getcwd())
+			lazy_load()
+
+			local current_dir = vim.fn.getcwd()
+			api.tree.toggle(false, false, current_dir)
 		end)
 		vim.keymap.set('n', '<Leader>nf', function()
+			lazy_load()
+
 			local curr_file = vim.fn.bufname()
-			api.tree.open()
+			api.tree.open(vim.fn.getcwd())
 			api.tree.find_file(curr_file)
 		end)
 		vim.keymap.set('n', '<Leader>nr', function ()
+			lazy_load()
 			api.tree.reload()
 		end)
 
-		nvim_tree.setup({})
 	end
 }
