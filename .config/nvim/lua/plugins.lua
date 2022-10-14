35e448d628d3ef13de556f229cecaf0593d11eba
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-14 13:00:47 -0400

    Tweaks packer config
    Packer now displays inside of a floating window, and it also profiles
    plugins by default.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,22 +3,25 @@ local packer = require('packer')
 -- Lists all non-hidden files in a provided directory
 local function ls_files(directory)
 	local to_return = {}
-	local process = io.popen('ls "'..directory..'"')
+	local process = io.popen('ls "' .. directory .. '"')
+	if process then
+		local i = 1;
+		for file in process:lines() do
+			to_return[i] = file
+			i = i + 1
+		end
 
-	local i = 1;
-	for file in process:lines() do
-		to_return[i] = file
-		i = i + 1
+		process:close()
+		return to_return
+	else
+		return {}
 	end
-
-	process:close()
-	return to_return
 end
 
-
+local packer_clone = nil
 local packer_install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
-	packerClone = vim.fn.system({
+	packer_clone = vim.fn.system({
 		'git',
 		'clone',
 		'--depth',
@@ -31,18 +34,27 @@ if vim.fn.empty(vim.fn.glob(packer_install_path)) > 0 then
 end
 
 local plugin_config_dir = os.getenv('HOME') .. '/.config/nvim/lua/plugins'
-local plugin_config_files = ls_files(plugin_config_dir);
-packer.startup(function (use)
-	-- Use each packer definition in the plugins folder
-	for i, pluginFile in ipairs(plugin_config_files) do
-		local pluginName = string.gsub(pluginFile, '.lua', '')
-		local pluginConfig = require('plugins/' .. pluginName)
-		use(pluginConfig)
-	end
+packer.startup({
+	function(use)
+		-- Use each packer definition in the plugins folder
+		local plugin_config_files = ls_files(plugin_config_dir);
+		for _, pluginFile in ipairs(plugin_config_files) do
+			local pluginName = string.gsub(pluginFile, '.lua', '')
+			local pluginConfig = require('plugins/' .. pluginName)
+			use(pluginConfig)
+		end
 
-	-- If this is a bootstrap run, automatically sync
-	if packerClone then
-		packer.sync()
-	end
-end)
-
+		-- If this is a bootstrap run, automatically sync
+		if packer_clone then
+			packer.sync()
+		end
+	end,
+	config = {
+		display = {
+			open_fn = require('packer.util').float
+		},
+		profile = {
+			enable = true
+		}
+	}
+})
