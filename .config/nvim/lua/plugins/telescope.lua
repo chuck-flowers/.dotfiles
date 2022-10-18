b8e95925a861f24823f9f950a7310d0cb93d9871
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-18 08:57:17 -0400

    Tweaks telescope keybinding

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,15 +2,17 @@ return {
 	'nvim-telescope/telescope.nvim',
 	requires = {
 		{ 'nvim-lua/plenary.nvim' },
-		{ 'kyazdani42/nvim-web-devicons' },
+		{ 'nvim-tree/nvim-web-devicons' },
 		{ 'nvim-telescope/telescope-project.nvim' }
 	},
 	config = function()
 		local telescope = require('telescope')
-
 		telescope.load_extension('project')
 
-		vim.keymap.set('n', '<Leader>ff', '<cmd>lua require("telescope.builtin").find_files({ hidden = true })<CR>')
+		vim.keymap.set('n', '<Leader>ff', function ()
+			require('telescope.builtin').find_files({ hidden = true })
+		end)
+
 		vim.keymap.set('n', '<Leader>fb', '<cmd>Telescope buffers<CR>')
 		vim.keymap.set('n', '<Leader>ft', '<cmd>Telescope live_grep<CR>')
 		vim.keymap.set('n', '<Leader>fh', '<cmd>Telescope help_tags<CR>')
