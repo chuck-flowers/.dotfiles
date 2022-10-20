aba9b59a557dc0e3282d045be6cd8d91688ed7d3
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-10-20 08:04:28 -0400

    Adds explicit gruvbox setup in nvim

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,6 +1,10 @@
 return {
 	'ellisonleao/gruvbox.nvim',
 	config = function()
+		local gruvbox = require 'gruvbox'
+
+		gruvbox.setup({})
+
 		vim.opt.termguicolors = true
 		vim.o.background = 'dark'
 		vim.cmd('colorscheme gruvbox')
