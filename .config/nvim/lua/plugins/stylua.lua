ae0015b46901e60b2fccc4ed5de62e75e290ab4c
Chuck Flowers <t_chuck_flowers@yahoo.com>
2022-12-02 09:35:12 -0500

    Reconfigures stylua plugin

--- .config/nvim/lua/plugins/stylua.lua
+++ .config/nvim/lua/plugins/stylua.lua
@@ -1,22 +1,25 @@
 return {
-	"wesleimp/stylua.nvim",
-	ft = { "lua" },
+	'ckipp01/stylua-nvim',
+	ft = { 'lua' },
 	requires = {
-		"nvim-lua/plenary.nvim",
+		'nvim-lua/plenary.nvim',
 	},
 	config = function()
-		local stylua = require("stylua")
+		local stylua = require 'stylua-nvim'
 
-		local auto_group = vim.api.nvim_create_augroup("Stylua", { clear = true })
-		vim.api.nvim_create_autocmd({
-			"BufWritePre",
-		}, {
-			pattern = { "*.lua" },
-			group = auto_group,
-			callback = function()
-				print("stylua auto command")
-				stylua.format()
-			end,
+		local autocmd_group = vim.api.nvim_create_augroup('Stylua', {
+			clear = true
 		})
+
+		vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+			group = autocmd_group,
+			pattern = { '*.lua' },
+			callback = function ()
+				print('Auto Command invoked')
+				stylua.format_file()
+			end
+		})
+
+		stylua.setup({})
 	end,
 }
