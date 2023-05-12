83f0882485388dcdba25d8c162261eb8a9300df2
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-05-12 08:17:51 -0400

    Moves some AwesomeWM config to dedicated modules

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -16,7 +16,6 @@ local hotkeys_popup = require 'awful.hotkeys_popup'
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require 'awful.hotkeys_popup.keys'
-local ruled = require 'ruled'
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -53,6 +52,9 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. 'gruvbox/theme.lua')
+
+require 'cflowers.rules'
+require 'cflowers.signals'
 
 -- This is used later as the default terminal and editor to run.
 local terminal = 'kitty'
@@ -325,50 +327,6 @@ local globalkeys = gears.table.join(
 	awful.key({}, 'XF86AudioPlay', function () awful.spawn('playerctl play-pause') end, { description = 'Play/Pause' })
 )
 
-local clientkeys = gears.table.join(
-    awful.key({ modkey,           }, 'f',
-        function (c)
-            c.fullscreen = not c.fullscreen
-            c:raise()
-        end,
-        {description = 'toggle fullscreen', group = 'client'}),
-    awful.key({ modkey,           }, 'w',      function (c) c:kill()                         end,
-              {description = 'close', group = 'client'}),
-    awful.key({ modkey, 'Control' }, 'space',  awful.client.floating.toggle                     ,
-              {description = 'toggle floating', group = 'client'}),
-    awful.key({ modkey, 'Control' }, 'Return', function (c) c:swap(awful.client.getmaster()) end,
-              {description = 'move to master', group = 'client'}),
-    awful.key({ modkey,           }, 'o',      function (c) c:move_to_screen()               end,
-              {description = 'move to screen', group = 'client'}),
-    awful.key({ modkey,           }, 't',      function (c) c.ontop = not c.ontop            end,
-              {description = 'toggle keep on top', group = 'client'}),
-    awful.key({ modkey,           }, 'n',
-        function (c)
-            -- The client currently has the input focus, so it cannot be
-            -- minimized, since minimized clients can't have the focus.
-            c.minimized = true
-        end ,
-        {description = 'minimize', group = 'client'}),
-    awful.key({ modkey,           }, 'm',
-        function (c)
-            c.maximized = not c.maximized
-            c:raise()
-        end ,
-        {description = '(un)maximize', group = 'client'}),
-    awful.key({ modkey, 'Control' }, 'm',
-        function (c)
-            c.maximized_vertical = not c.maximized_vertical
-            c:raise()
-        end ,
-        {description = '(un)maximize vertically', group = 'client'}),
-    awful.key({ modkey, 'Shift'   }, 'm',
-        function (c)
-            c.maximized_horizontal = not c.maximized_horizontal
-            c:raise()
-        end ,
-        {description = '(un)maximize horizontally', group = 'client'})
-)
-
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it work on any keyboard layout.
 -- This should map on the top row of your keyboard, usually 1 to 9.
@@ -419,93 +377,9 @@ for i = 1, 9 do
     )
 end
 
-local clientbuttons = gears.table.join(
-    awful.button({ }, 1, function (c)
-        c:emit_signal('request::activate', 'mouse_click', {raise = true})
-    end),
-    awful.button({ modkey }, 1, function (c)
-        c:emit_signal('request::activate', 'mouse_click', {raise = true})
-        awful.mouse.client.move(c)
-    end),
-    awful.button({ modkey }, 3, function (c)
-        c:emit_signal('request::activate', 'mouse_click', {raise = true})
-        awful.mouse.client.resize(c)
-    end)
-)
-
 -- Set keys
 root.keys(globalkeys)
 -- }}}
-
--- All clients will match this rule.
-ruled.client.append_rule {
-	rule = { },
-	properties = {
-		border_width = beautiful.border_width,
-		border_color = beautiful.border_normal,
-		focus = awful.client.focus.filter,
-		raise = true,
-		keys = clientkeys,
-		buttons = clientbuttons,
-		screen = awful.screen.preferred,
-		placement = awful.placement.no_overlap+awful.placement.no_offscreen
-	}
-}
-
--- Floating clients.
-ruled.client.append_rule {
-	rule_any = {
-        instance = {
-          'DTA',  -- Firefox addon DownThemAll.
-          'copyq',  -- Includes session name in class.
-          'pinentry',
-        },
-        class = {
-          'Arandr',
-          'Blueman-manager',
-          'Gpick',
-          'Kruler',
-          'MessageWin',  -- kalarm.
-          'Sxiv',
-          'Tor Browser', -- Needs a fixed window size to avoid fingerprinting by screen size.
-          'Wpa_gui',
-          'veromix',
-          'xtightvncviewer'},
-
-        -- Note that the name property shown in xprop might be set slightly after creation of the client
-        -- and the name shown there might not match defined rules here.
-        name = {
-          'Event Tester',  -- xev.
-        },
-        role = {
-          'AlarmWindow',  -- Thunderbird's calendar.
-          'ConfigManager',  -- Thunderbird's about:config.
-          'pop-up',       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-	},
-	properties = {
-
-	}
-}
-
--- Add titlebars to normal clients and dialogs
-ruled.client.append_rule {
-	rule_any = {
-		type = { 'normal', 'dialog' }
-	},
-	properties = {
-		titlebars_enabled = true
-	}
-}
-
-ruled.client.append_rule {
-	rule = {
-		class = "steam"
-	},
-	properties = {
-		titlebars_enabled = false
-	}
-}
 
 -- {{{ Signals
 -- Signal function to execute when a new client appears.
