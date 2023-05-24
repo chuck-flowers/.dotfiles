9e3f5d7739c2b59969364857f124fb42fa9a917b
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-05-24 14:44:36 -0400

    Adds a CSV previewer

---
+++ .config/nvim/lua/plugins/preview-csv.lua
@@ -0,0 +1,12 @@
+return {
+	'Nguyen-Hoang-Nam/nvim-preview-csv',
+	ft = { 'csv' },
+	config = function()
+		local preview_csv = require('nvim-preview-csv')
+
+		vim.api.nvim_buf_create_user_command(0, 'PreviewCsv', preview_csv.preview, {})
+
+		preview_csv.setup({
+		})
+	end
+}
