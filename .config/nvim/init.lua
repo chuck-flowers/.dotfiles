aeafbcdced58ccdc0c7b658144b8a7a97f93d986
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-06-14 08:28:58 -0400

    Adds a border to lazy.nvim window

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -46,5 +46,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup('plugins')
+require('lazy').setup('plugins', {
+	ui = {
+		border = 'rounded'
+	}
+})
 
