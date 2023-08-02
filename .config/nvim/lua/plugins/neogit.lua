14918a39279930d418585b6d745e15f5a0dcebb6
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-08-02 10:11:51 -0400

    Add telescope dependency to neogit

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,22 +1,28 @@
 return {
 	'NeogitOrg/neogit',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'nvim-telescope/telescope.nvim'
+	},
 	config = function()
 		require('neogit').setup()
 	end,
 	keys = {
 		{
-			'<leader>gs', function()
+			'<leader>gs',
+			function()
 				require('neogit').open()
 			end
 		},
 		{
-			'<leader>gc', function()
+			'<leader>gc',
+			function()
 				require("neogit").open({ "commit" })
 			end
 		},
 		{
-			'<leader>gl', function()
+			'<leader>gl',
+			function()
 				require("neogit").open({ "log" })
 			end
 		}
