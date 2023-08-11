0aa52fd124cdd830881f5966dfc012a3e5e0a9d9
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-08-11 14:28:57 -0400

    Add diffview to `neogit`

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -2,7 +2,8 @@ return {
 	'NeogitOrg/neogit',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
-		'nvim-telescope/telescope.nvim'
+		'nvim-telescope/telescope.nvim',
+		'sindrets/diffview.nvim'
 	},
 	config = function()
 		require('neogit').setup()
