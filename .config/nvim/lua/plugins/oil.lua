678831552c499b61cf2694e47edefab9871e946d
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-08-21 09:06:44 -0400

    Install and configure `oil.nvim`

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,18 @@
+return {
+	'stevearc/oil.nvim',
+	dependencies = {
+		"nvim-tree/nvim-web-devicons"
+	},
+	config = function()
+		local oil = require('oil')
+
+		oil.setup()
+	end,
+	keys = {
+		{
+			'<leader>o.', function()
+				require('oil').open_float()
+			end
+		}
+	}
+}
