c48e8bd2d929b5800d7bd7309d29bfdd997f0763
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-09-26 21:21:10 -0400

    Add border to LSP hover

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -64,6 +64,13 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities.textDocument.completion.completionItem.snippetSupport = true
 
+		vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+			vim.lsp.handlers.hover, {
+				title = 'hover',
+				border = 'single'
+			}
+		)
+
 		lspconfig.awk_ls.setup({})
 		lspconfig.bashls.setup({})
 		lspconfig.cssls.setup({ capabilities = capabilities })
