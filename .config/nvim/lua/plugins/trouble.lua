3feb9ac2b761181d55b493bb4308c2398cbf5772
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-09-26 21:21:28 -0400

    Add telescope key binding to trouble.nvim

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -10,6 +10,7 @@ return {
 		{ '<leader>vv', function() require('trouble').toggle() end },
 		{ '<leader>vd', function() require('trouble').open('workspace_diagnostics') end },
 		{ '<leader>vq', function() require('trouble').open('quickfix') end },
+		{ '<leader>vt', function() require('trouble').open('telescope') end },
 		{ '<C-h>',      function() require('trouble').previous({ skip_groups = true, jump = true }) end },
 		{ '<C-l>',      function() require('trouble').next({ skip_groups = true, jump = true }) end },
 	}
