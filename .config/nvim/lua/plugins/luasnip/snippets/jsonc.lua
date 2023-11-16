1fb9abef72193b890dd013bb86340938ac3e2db9
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-11-16 10:37:18 -0500

    Copy the json snippets to jsonc as well

---
+++ .config/nvim/lua/plugins/luasnip/snippets/jsonc.lua
@@ -0,0 +1,17 @@
+return {
+	s('eslintrc', fmt([[
+		{{
+			"$schema": "https://json.schemastore.org/eslintrc.json"
+		}}
+	]], {})),
+	s('npm-package', fmt([[
+		{{
+			"$schema": "https://json.schemastore.org/package.json"
+		}}
+	]], {})),
+	s('tsconfig', fmt([[
+		{{
+			"$schema": "https://json.schemastore.org/tsconfig.json"
+		}}
+	]], {})),
+}
