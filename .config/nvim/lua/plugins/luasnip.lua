f7660482f2049e544270ce9570d94031cce57bc0
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-11-22 10:03:00 -0500

    Adds choice cycling keybindings to luasnip

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -1,29 +1,33 @@
 return {
 	'L3MON4D3/LuaSnip',
 	config = function()
-		-- Modules
-		local luasnip = require('luasnip')
-		local luasnip_lua_loader = require('luasnip.loaders.from_lua')
-
 		-- Load the snippets from files
 		local snippet_dir = vim.fn.stdpath('config') .. '/lua/plugins/luasnip/snippets'
-		luasnip_lua_loader.load({
+		require('luasnip.loaders.from_lua').load({
 			paths = snippet_dir
 		})
 
 		local function safe_jump(offset)
-			if luasnip.jumpable(offset) then
-				luasnip.jump(offset)
+			if require('luasnip').jumpable(offset) then
+				require('luasnip').jump(offset)
+			end
+		end
+
+		local function safe_choice(offset)
+			if require('luasnip').choice_active() then
+				require('luasnip').change_choice(offset)
 			end
 		end
 
 		local function safe_jump_next() safe_jump(1) end
 		local function safe_jump_prev() safe_jump(-1) end
+		local function safe_choice_next() safe_choice(1) end
+		local function safe_choice_prev() safe_choice(-1) end
 
 		-- Key Maps
 		vim.keymap.set({ 'i', 's' }, '<c-l>', safe_jump_next, { silent = true })
 		vim.keymap.set({ 'i', 's' }, '<c-h>', safe_jump_prev, { silent = true })
-
+		vim.keymap.set({ 'i', 's' }, '<c-j>', safe_choice_next, { silent = true })
+		vim.keymap.set({ 'i', 's' }, '<c-k>', safe_choice_prev, { silent = true })
 	end
 }
-
