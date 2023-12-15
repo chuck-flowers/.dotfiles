017da02e4feec9254eca193f38d8eef879aa7d06
Chuck Flowers <t_chuck_flowers@yahoo.com>
2023-12-15 15:23:42 -0500

    Delete yaml snippets

--- .config/nvim/lua/plugins/luasnip/snippets/yaml.lua
+++
@@ -1,7 +0,0 @@
-return {
-	s('CloudFormation', fmt([[
-		# yaml-language-server: $schema=https://raw.githubusercontent.com/awslabs/goformation/master/schema/cloudformation.schema.json
-		AWSTemplateFormatVersion: "2010-09-09"
-		Resources:
-	]], {}))
-}
