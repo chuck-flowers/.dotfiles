4f3c8a6883602924855416da9e1309b24ed84719
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-04-25 18:34:03 -0400

    Add `header` snippet for dockerfile

---
+++ .config/nvim/lua/plugins/luasnip/snippets/dockerfile.lua
@@ -0,0 +1,15 @@
+return {
+	s('header', fmt([[
+		{leading}
+		# {title} #
+		{trailing}
+	]], {
+		leading = f(function(args)
+			return string.rep('#', string.len(args[1][1]) + 4)
+		end, { 1 }),
+		title = i(1, 'TITLE'),
+		trailing = f(function(args)
+			return string.rep('#', string.len(args[1][1]) + 4)
+		end, { 1 })
+	}))
+}
