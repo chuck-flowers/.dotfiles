144146fdb0d39f2f5cbe8c08b19eac84110fd0e1
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-05-30 16:57:28 -0400

    Use plugins.lsp.utils for helm_ls setup

--- .config/nvim/lua/plugins/lsp/helm_ls.lua
+++ .config/nvim/lua/plugins/lsp/helm_ls.lua
@@ -1,9 +1,6 @@
 require('lspconfig').helm_ls.setup({
 	capabilities = require('plugins.lsp.utils').client_capabilities,
-	on_attach = function(_, bufnr)
-		common_keybindings()
-		register_autoformat(bufnr, 'Helm')
-	end,
+	on_attach = require('plugins.lsp.utils').on_attach,
 	settings = {
 		['helm-ls'] = {
 			yamlls = {
