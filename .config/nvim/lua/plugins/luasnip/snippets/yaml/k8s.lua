f84581430618f536c8b5fff04466ffe1aa50144b
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-06-24 09:13:06 -0400

    Add yaml snippets

---
+++ .config/nvim/lua/plugins/luasnip/snippets/yaml/k8s.lua
@@ -0,0 +1,14 @@
+return {
+	s('k8s:metadata', fmta([[
+		metadata:
+		  name: '<name>'
+		  labels:
+		    app.kubernetes.io/name: '{{ .Chart.Name }}'
+		    app.kubernetes.io/component: '<component>'
+		    app.kubernetes.io/instance: '{{ .Release.Name }}'
+		    app.kubernetes.io/version: '{{ .Chart.AppVersion }}'
+	]], {
+		name = i(1, 'name'),
+		component = i(2, 'component')
+	}))
+}
