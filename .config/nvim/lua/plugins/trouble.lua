9a9a3e92c122d0ff97a74e4e4e08a436e6ef4015
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-06-24 09:12:33 -0400

    Fix trouble config for nvim

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -8,10 +8,16 @@ return {
 	},
 	keys = {
 		{ '<leader>vv', function() require('trouble').toggle() end },
-		{ '<leader>vd', function() require('trouble').open('workspace_diagnostics') end },
-		{ '<leader>vq', function() require('trouble').open('quickfix') end },
-		{ '<leader>vt', function() require('trouble').open('telescope') end },
-		{ '<C-h>',      function() require('trouble').previous({ skip_groups = true, jump = true }) end },
-		{ '<C-l>',      function() require('trouble').next({ skip_groups = true, jump = true }) end },
+		{ '<leader>vd', ':Trouble diagnostics<CR>' },
+		{ '<leader>vq', ':Trouble qflist<CR>' },
+		{ '<leader>vt', ':Trouble telescope<CR>' },
+		{ '<C-h>', function()
+			require('trouble').prev()
+			require('trouble').jump()
+		end },
+		{ '<C-l>', function()
+			require('trouble').next()
+			require('trouble').jump()
+		end },
 	}
 }
