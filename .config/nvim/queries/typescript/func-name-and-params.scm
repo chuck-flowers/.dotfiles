5c924186ef072bff4805dde11d62c1caee734fe8
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-06-24 09:11:36 -0400

    Refine the TS Query for func name and params

--- .config/nvim/queries/typescript/func-name-and-params.scm
+++ .config/nvim/queries/typescript/func-name-and-params.scm
@@ -2,13 +2,16 @@
 ;; Move cursor to a capture ("@foo") to highlight matches in the source buffer.
 ;; Completion for grammar nodes is available (:help compl-omni)
 
-
 [
  (method_definition
+   name: (_) @name)
+ (method_definition
    name: (_) @name
-   parameters: (formal_parameters (required_parameter (identifier) @params)*))
+   parameters: (formal_parameters (required_parameter (identifier)* @params)))
+ (function_declaration
+   name: (_) @name)
  (function_declaration
    name: (_) @name
-   parameters: (formal_parameters (required_parameter (identifier) @params)*))
+   parameters: (formal_parameters (required_parameter (identifier)* @params)))
 ]
 
