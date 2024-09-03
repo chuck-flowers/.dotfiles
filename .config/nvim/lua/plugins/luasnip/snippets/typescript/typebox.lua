dcb4d75820c928e00e8343b1ffee684f04e674e3
Chuck Flowers <t_chuck_flowers@yahoo.com>
2024-09-03 08:34:05 -0400

    Add typebox snippets to neovim

---
+++ .config/nvim/lua/plugins/luasnip/snippets/typescript/typebox.lua
@@ -0,0 +1,29 @@
+local object = s('typebox:object', fmt([[
+	Type.Object({{
+		{body}
+	}})
+]], {
+	body = i(0)
+}))
+
+local object_static = s('typebox:object:static', fmt([[
+	type {type_name} = Static<typeof {name_ref}>;
+	const {name} = Type.Object({{
+		{body}
+	}})
+]], {
+	name = i(1),
+	body = i(2),
+	name_ref = rep(1),
+	type_name = f(function(args)
+		--- @type string
+		local name = args[1][1]
+
+		return name:sub(1, 1):upper() .. name:sub(2)
+	end, { 1 }),
+}))
+
+return {
+	object,
+	object_static,
+}
