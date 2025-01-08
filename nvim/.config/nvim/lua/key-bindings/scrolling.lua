e028b01b201c37338c2bdaf53497b2c953688310
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-01-08 09:31:44 -0500

    feat(nvim): Center "go to diagnostic" result

--- nvim/.config/nvim/lua/key-bindings/scrolling.lua
+++ nvim/.config/nvim/lua/key-bindings/scrolling.lua
@@ -1,6 +1,17 @@
-vim.keymap.set("n", "<C-f>", "<C-f>M", { noremap = true })
-vim.keymap.set("n", "<C-b>", "<C-b>M", { noremap = true })
-vim.keymap.set("n", "<C-d>", "<C-d>M", { noremap = true })
-vim.keymap.set("n", "<C-u>", "<C-u>M", { noremap = true })
+vim.keymap.set("n", "<C-f>", "<C-f>M")
+vim.keymap.set("n", "<C-b>", "<C-b>M")
+vim.keymap.set("n", "<C-d>", "<C-d>M")
+vim.keymap.set("n", "<C-u>", "<C-u>M")
+
 vim.keymap.set("n", "n", "nzz")
 vim.keymap.set("n", "N", "Nzz")
+
+vim.keymap.set("n", "]d", function()
+	vim.diagnostic.goto_next()
+	vim.cmd('norm zz')
+end)
+
+vim.keymap.set("n", "[d", function()
+	vim.diagnostic.goto_prev()
+	vim.cmd('norm zz')
+end)
