7db19e076ca7b45796394e8795118ec689f976bd
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-01-08 09:32:16 -0500

    feat(nvim): Add quickfix keybindings

---
+++ nvim/.config/nvim/lua/key-bindings/quickfix.lua
@@ -0,0 +1,26 @@
+local function toggle_qf()
+	local is_open = false
+
+	local win_ids = vim.api.nvim_list_wins()
+	for _, win_id in ipairs(win_ids) do
+		local buf_id = vim.api.nvim_win_get_buf(win_id)
+		local ft = vim.fn.getbufvar(buf_id, '&filetype')
+		if ft == 'qf' then
+			is_open = true
+			break
+		end
+	end
+
+	if is_open then
+		vim.cmd('cclose')
+	else
+		vim.cmd('copen')
+	end
+end
+
+vim.keymap.set('n', '<leader>vv', toggle_qf)
+vim.keymap.set('n', '<leader>vd', function()
+	vim.diagnostic.setqflist({ open = true })
+end)
+vim.keymap.set('n', ']q', '<cmd>:cnext<cr>zz')
+vim.keymap.set('n', '[q', '<cmd>:cprev<cr>zz')
