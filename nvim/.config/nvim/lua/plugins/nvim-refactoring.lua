85bfce37df1c8fa246036d7fa723052fe93538b7
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-03-19 09:38:53 -0400

    chore(nvim): Fix the refactoring plugin

--- nvim/.config/nvim/lua/plugins/nvim-refactoring.lua
+++ nvim/.config/nvim/lua/plugins/nvim-refactoring.lua
@@ -19,25 +19,9 @@ return {
 		"typescriptreact",
 	},
 	config = function()
-		local refactoring = require("refactoring")
+		require("refactoring").setup({})
 
-		refactoring.setup({})
-
-		local extract_function = function()
-			refactoring.refactor("Extract Function")
-		end
-
-		local extract_variable = function()
-			refactoring.refactor("Extract Variable")
-		end
-
-		local inline_variable = function()
-			refactoring.refactor("Inline Variable")
-		end
-
-		vim.keymap.set("v", "<leader>re", extract_function)
-		vim.keymap.set("v", "<leader>rv", extract_variable)
-		vim.keymap.set("v", "<leader>ri", inline_variable)
-		vim.keymap.set("n", "<leader>ri", inline_variable)
+		vim.keymap.set("v", "<leader>rv", "<cmd>Refactor extract_var<cr>")
+		vim.keymap.set({ "n", "x" }, "<leader>ri", "<cmd>Refactor inline_var<cr>")
 	end,
 }
