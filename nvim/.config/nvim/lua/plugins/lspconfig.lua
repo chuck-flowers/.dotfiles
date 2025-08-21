622c7705a52a07842c9f51d2553c3374070d4aa7
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-08-20 23:01:34 -0400

    chore(nvim): Remove deprecated hover handler

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -14,14 +14,9 @@ return {
 
 		require("lspconfig.ui.windows").default_options.border = "single"
 
-		vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-			title = "hover",
-			border = "single",
-		})
-
+		-- Load all customized configs
 		local lsp_config_dir = vim.fn.stdpath("config") .. "/lua/plugins/lsp"
 		local files = vim.split(vim.fn.glob(lsp_config_dir .. "/*"), "\n", { trimempty = true })
-
 		for _, file in ipairs(files) do
 			local mod_file = file:sub(#lsp_config_dir + 2)
 			local mod_name = mod_file:sub(0, #mod_file - 4)
@@ -31,4 +26,3 @@ return {
 		end
 	end,
 }
-
