ff8a7257a57442f4edaa1e80373f6fec43c271bf
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-08-20 23:03:59 -0400

    fix(nvim): Update the ESLint LSP config

--- nvim-web/.config/nvim/lua/plugins/lsp/eslint.lua
+++ nvim-web/.config/nvim/lua/plugins/lsp/eslint.lua
@@ -1,14 +1,21 @@
-require("lspconfig").eslint.setup({
+vim.lsp.enable('eslint')
+vim.lsp.config('eslint', {
 	on_attach = function(client, bufnr)
-		require("plugins.lsp.utils").on_attach(client, bufnr)
-
 		vim.api.nvim_create_autocmd("BufWritePre", {
 			buffer = bufnr,
 			desc = "Auto format file with " .. client.name,
-			command = ":EslintFixAll",
+			callback = function()
+				client:exec_cmd({
+					title = 'Fix all Eslint errors for current buffer',
+					command = 'eslint.applyAllFixes',
+					arguments = {
+						{
+							uri = vim.uri_from_bufnr(bufnr),
+							version = vim.lsp.util.buf_versions[bufnr],
+						},
+					},
+				}, { bufnr = bufnr })
+			end
 		})
 	end,
-	settings = {
-		format = true,
-	},
 })
