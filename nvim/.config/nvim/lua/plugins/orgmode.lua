c4efecf0ea8cbf37133439a597322a6d5e7bbc47
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-09-16 07:00:28 -0400

    chore(nvim/org): Focus the file globbing

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("orgmode").setup({
 			calendar_week_start_day = 0,
-			org_agenda_files = "~/org/**/*",
+			org_agenda_files = "~/org/**/*.org",
 			org_agenda_span = "day",
 			org_agenda_skip_deadline_if_done = true,
 			org_agenda_skip_scheduled_if_done = true,
