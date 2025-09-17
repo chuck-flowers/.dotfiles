21122386db74be1c7e2d243acfbc161890315713
Chuck Flowers <t_chuck_flowers@yahoo.com>
2025-09-17 07:14:06 -0400

    fix(nvim): Remove bad/unused YAML schema

--- nvim/.config/nvim/lua/plugins/lsp/yamlls.lua
+++ nvim/.config/nvim/lua/plugins/lsp/yamlls.lua
@@ -20,7 +20,6 @@ vim.lsp.config('yamlls', {
 				"*.cf.yml",
 				kubernetes = "*.k8s.yml",
 				["https://json.schemastore.org/prometheus.json"] = "prometheus.yml",
-				["https://json.schemastore.org/github-workflow.json"] = "/.github/worksflows/*.yml"
 			},
 		},
 	},
