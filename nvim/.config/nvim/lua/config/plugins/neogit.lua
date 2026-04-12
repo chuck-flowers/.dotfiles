1691b434c48500efa14330b5e5e6952161f31e91
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-04-12 14:06:40 -0400

    Port neogit

--- nvim/.config/nvim/lua/config/plugins/neogit.lua
+++ nvim/.config/nvim/lua/config/plugins/neogit.lua
@@ -2,3 +2,10 @@ vim.pack.add({
 	'https://github.com/nvim-lua/plenary.nvim',
 	'https://github.com/NeogitOrg/neogit',
 })
+
+require('neogit').setup({
+
+})
+
+vim.keymap.set('n', '<leader>gs', ':Neogit<cr>')
+
