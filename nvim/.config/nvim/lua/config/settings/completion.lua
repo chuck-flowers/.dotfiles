98aa57dd0beefd7a25b7e692d6f1897c50223535
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-04-15 08:50:25 -0400

    Configure LSP

--- nvim/.config/nvim/lua/config/settings/completion.lua
+++ nvim/.config/nvim/lua/config/settings/completion.lua
@@ -1,3 +1,21 @@
 vim.opt.autocomplete = true
-vim.opt.complete = { '.^5', 'w^5', 'b^5', 'u^5' }
-vim.opt.completeopt = 'popup'
+vim.opt.complete = { '.^3', 'w^3', 'b^3', 'u^3' }
+vim.opt.completeopt = { 'menuone', 'noselect', 'popup' }
+
+local group_id = vim.api.nvim_create_augroup('completion.settings', {
+	clear = true,
+})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = group_id,
+	callback = function (ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if not client then
+			return
+		end
+
+		vim.lsp.completion.enable(true, client.id, ev.buf, {
+			autotrigger = true,
+		})
+	end
+})
