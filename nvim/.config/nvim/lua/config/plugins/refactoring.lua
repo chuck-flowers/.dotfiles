3b598de406e22a3e9a16a84fded69016b01320c3
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-05-02 10:58:24 -0400

    fix(nvim): Use correct `refactoring.nvim` deps

--- nvim/.config/nvim/lua/config/plugins/refactoring.lua
+++ nvim/.config/nvim/lua/config/plugins/refactoring.lua
@@ -1,10 +1,7 @@
-vim.pack.add({
-	"https://github.com/ThePrimeagen/refactoring.nvim",
-	"https://github.com/nvim-lua/plenary.nvim",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-})
-
-require("refactoring").setup({})
+vim.pack.add {
+  "https://github.com/lewis6991/async.nvim",
+  "https://github.com/theprimeagen/refactoring.nvim"
+}
 
 vim.keymap.set("v", "<leader>rv", "<cmd>Refactor extract_var<cr>")
 vim.keymap.set({ "n", "x" }, "<leader>ri", "<cmd>Refactor inline_var<cr>")
