1034dada1af43d2e37c7efc4c767a2f95cfd54b7
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-05-02 10:58:00 -0400

    feat(nvim): Apply preferences to orgmode

--- nvim/.config/nvim/lua/config/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/config/plugins/orgmode.lua
@@ -5,6 +5,14 @@ vim.pack.add({
 require('orgmode').setup({
 	org_agenda_files = '~/org/**/*.org',
 	org_default_notes_file = '~/org/notes.org',
+	org_startup_indented = true,
+	org_adapt_indentation = false,
+	org_indent_mode_turns_on_hiding_stars = false,
+	org_custom_exports = {
+		-- TODO
+	},
+	calendar_week_start_day = 0,
+	org_agenda_span = 'day',
 })
 
 vim.keymap.set('n', '<leader>fo', function()
