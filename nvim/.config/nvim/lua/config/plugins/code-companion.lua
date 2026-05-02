29c7094826999efd581d76a816f456ed802e9ec5
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-05-02 10:58:57 -0400

    feat(nvim): Add openrouter to codecompanion.nvim

--- nvim/.config/nvim/lua/config/plugins/code-companion.lua
+++ nvim/.config/nvim/lua/config/plugins/code-companion.lua
@@ -7,25 +7,46 @@ require('codecompanion').setup({
 	adapters = {
 		http = {
 			ollama = function ()
-				return require('codecompanion.adapters').extend('ollama', {
-					-- TODO: Settings
+				return require('codecompanion.adapters').extend('ollama')
+			end,
+			openrouter = function()
+				return require('codecompanion.adapters').extend('openai_compatible', {
+					env = {
+						url = 'https://openrouter.ai/api',
+						api_key = 'OPENROUTER_API_KEY',
+						chat_url = '/v1/chat/completions',
+					},
+					schema = {
+						model = {
+							default = 'openai/gpt-oss-120b:free'
+						},
+					},
 				})
 			end
 		}
 	},
 	interactions = {
 		chat = {
-			adapter = {
-				name = 'ollama',
-				model = 'qwen3.5',
-			},
+			-- adapter = { name = 'ollama', model = 'qwen3.5' },
+			adapter = 'openrouter',
+			-- adapter = { name = 'openrouter', model = 'openrouter/auto' },
 			keymaps = {
 				completion = {
 					modes = { i = '<C-Space>' },
 				},
 			},
 		},
+		inline = {
+			-- adapter = { name = 'ollama', model = 'qwen3.5' },
+			adapter = 'openrouter',
+			-- adapter = { name = 'openrouter', model = 'openrouter/auto' },
+		},
+	},
+	opts = {
+		log_level = 'DEBUG',
 	},
 })
 
-vim.keymap.set('n', '<leader>aa', function() require('codecompanion').toggle() end)
+-- vim.keymap.set('n', '<leader>aa', function() require('codecompanion').actions() end)
+vim.keymap.set('n', '<leader>ac', function() require('codecompanion').toggle() end)
+vim.keymap.set({ 'n', 'v' }, '<leader>ai', ':CodeCompanion<cr>')
