91db4128169b832dbe80660129b9b8e8ff82b0f7
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-05-02 10:57:37 -0400

    feat(nvim): Add keymaps for TS text objects

--- nvim/.config/nvim/lua/config/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/config/plugins/treesitter.lua
@@ -8,3 +8,18 @@ require('nvim-treesitter-textobjects').setup({
 
 })
 
+vim.keymap.set({ "x", "o" }, "af", function()
+	require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "if", function()
+	require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ac", function()
+	require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+end)
+
+vim.keymap.set({ "x", "o" }, "ic", function()
+	require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+end)
