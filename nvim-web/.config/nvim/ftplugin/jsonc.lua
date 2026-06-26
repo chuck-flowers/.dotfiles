40f451674bb5a688580b34e0e7dc3b2bb1eae4a9
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-06-26 09:06:59 -0400

    chore(nvim): Add `oxfmt` to JSON files

--- nvim-web/.config/nvim/ftplugin/jsonc.lua
+++ nvim-web/.config/nvim/ftplugin/jsonc.lua
@@ -1,6 +1,8 @@
 -- Configure LSP
-local LSP = 'jsonls'
-if not vim.lsp.is_enabled(LSP) then
-	vim.lsp.enable(LSP)
+for _, lsp in ipairs({ 'jsonls', 'oxfmt' }) do
+	if not vim.lsp.is_enabled(lsp) then
+		vim.lsp.enable(lsp)
+	end
 end
 
+vim.keymap.set('n', '<leader>rf', ":%!jq -r '.'<left>", { buf = 0 })
