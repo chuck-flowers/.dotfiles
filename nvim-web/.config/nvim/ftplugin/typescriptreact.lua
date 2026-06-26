0206f2cb156ef5cc9faa54d0b0a4be1b54874a30
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-06-26 09:05:51 -0400

    feat(nvim): Register more LSPs for TSX

--- nvim-web/.config/nvim/ftplugin/typescriptreact.lua
+++ nvim-web/.config/nvim/ftplugin/typescriptreact.lua
@@ -3,7 +3,7 @@ require('nvim-treesitter').install({ 'tsx' }):wait(10 * 1000)
 vim.treesitter.start()
 
 -- Configure LSP
-for _, lsp in ipairs({ 'ts_ls', 'eslint', 'oxlint' }) do
+for _, lsp in ipairs({ 'emmet_language_server', 'ts_ls', 'eslint', 'oxlint', 'oxfmt' }) do
 	if not vim.lsp.is_enabled(lsp) then
 		require('utils.lsp').enable_lsp(lsp)
 	end
