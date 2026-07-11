c8d83f141e52526d18e9ed86efc2def78f6d5189
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-07-11 13:53:08 -0400

    feat(nvim): Configure dockerfile settings

--- nvim-docker/.config/nvim/ftplugin/dockerfile.lua
+++ nvim-docker/.config/nvim/ftplugin/dockerfile.lua
@@ -1,10 +1,9 @@
 -- Treesitter
-require('nvim-treesitter').install({ 'dockerfile' }):wait(10 * 1000)
+require('nvim-treesitter').install({ 'bash' }):wait(10 * 1000)
 vim.treesitter.start()
 
 -- Configure LSP
-local LSP = 'dockerls'
+local LSP = 'bashls'
 if not vim.lsp.is_enabled(LSP) then
 	require('utils.lsp').enable_lsp(LSP)
 end
-
