abbe511663a86dfa665be4dbf42d83672083070b
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-07-11 13:55:23 -0400

    feat(nvim): Configure CSS settings

--- nvim-web/.config/nvim/ftplugin/css.lua
+++ nvim-web/.config/nvim/ftplugin/css.lua
@@ -1,6 +1,10 @@
+-- Treesitter
+require('nvim-treesitter').install({ 'css' }):wait(10 * 1000)
+vim.treesitter.start()
+
 -- Config LSPs
-for _, lsp in ipairs({ 'cssls', 'stylelint_lsp' }) do
+for _, lsp in ipairs({ 'cssls', 'stylelint_lsp', 'oxfmt' }) do
 	if not vim.lsp.is_enabled(lsp) then
-		vim.lsp.enable(lsp)
+		require('utils.lsp').enable_lsp(lsp)
 	end
 end
