27ecd4da312fc57688e7d645152fc1af0851cc32
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-08-03 08:51:04 -0400

    feat(nvim): Configure markdown LSP/Treesitter

--- nvim/.config/nvim/ftplugin/markdown.lua
+++ nvim/.config/nvim/ftplugin/markdown.lua
@@ -1,2 +1,12 @@
 -- Auto wrap text at 80 characters
 vim.bo.textwidth = 80
+
+-- Treesitter
+require('nvim-treesitter').install({ 'markdown' }):wait(10 * 1000)
+vim.treesitter.start()
+
+-- Configure LSP
+local LSP = 'marksman'
+if not vim.lsp.is_enabled(LSP) then
+	require('utils.lsp').enable_lsp(LSP)
+end
