664ca543590d9c191649c78335f6297c9e01e938
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-08-03 08:58:20 -0400

    feat(nvim): Show permissions column in oil

--- nvim/.config/nvim/lua/config/plugins/oil.lua
+++ nvim/.config/nvim/lua/config/plugins/oil.lua
@@ -1,9 +1,13 @@
 vim.pack.add({ 'https://github.com/stevearc/oil.nvim' })
 
 require('oil').setup({
+	columns = {
+		'icon',
+		'permissions',
+	},
 	view_options = {
 		show_hidden = true,
-	}
+	},
 })
 
 vim.keymap.set('n', '<leader>xx', ':Oil<cr>')
