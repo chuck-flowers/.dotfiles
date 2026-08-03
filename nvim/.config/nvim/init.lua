b9abca64bcbe474adc3f28171f1cb4f762546297
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-08-03 06:18:36 -0400

    chore(nvim): Apply settings after plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,15 +4,6 @@ vim.g.mapleader = " "
 
 local ok, err
 
--- Load Settings
-local settings_mods = fs_utils.ls_sub_modules('config.settings')
-for _, settings_mod in pairs(settings_mods) do
-	ok, err = pcall(require, settings_mod)
-	if not ok then
-		vim.notify(vim.inspect(err), vim.log.levels.ERROR)
-	end
-end
-
 -- Load Plugins
 local plugin_mods = fs_utils.ls_sub_modules('config.plugins')
 for _, plugin_mod in pairs(plugin_mods) do
@@ -22,3 +13,11 @@ for _, plugin_mod in pairs(plugin_mods) do
 	end
 end
 
+-- Load Settings
+local settings_mods = fs_utils.ls_sub_modules('config.settings')
+for _, settings_mod in pairs(settings_mods) do
+	ok, err = pcall(require, settings_mod)
+	if not ok then
+		vim.notify(vim.inspect(err), vim.log.levels.ERROR)
+	end
+end
