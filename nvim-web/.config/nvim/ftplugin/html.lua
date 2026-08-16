a1d20e3a823908166996a6dd8bca1ad58943157d
Chuck Flowers <t_chuck_flowers@yahoo.com>
2026-08-16 13:24:13 -0400

    fix(nvim): Fix treesitter for HTML files

--- nvim-web/.config/nvim/ftplugin/html.lua
+++ nvim-web/.config/nvim/ftplugin/html.lua
@@ -1,4 +1,8 @@
--- Config LSPs
+-- Treesitter
+require('nvim-treesitter').install({ 'html' }):wait(10 * 1000)
+vim.treesitter.start()
+
+-- LSP
 for _, lsp in ipairs({ 'emmet_language_server', 'html' }) do
 	if not vim.lsp.is_enabled(lsp) then
 		require('utils.lsp').enable_lsp(lsp)
